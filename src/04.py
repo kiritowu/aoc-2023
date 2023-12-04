5e7f7c0a4900f46cc0cce8696a579369fb5727ad
Wong Zhao Wu, Bryan <70313929+kiritowu@users.noreply.github.com>
2023-12-04 02:36:50 +0000

    Add day 4 boilepte

---
+++ src/04.py
@@ -0,0 +1,9 @@
+from typing import List
+
+def part1(lines: List[str]):
+    pass
+
+if __name__ == "__main__":
+    with open("../inputs/04.txt", "r") as f:
+        inputs = f.read().splitlines()
+    part1(inputs)
