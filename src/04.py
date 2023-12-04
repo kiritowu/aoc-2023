38edaf82dace50be9e6c22c88356b26154339e26
Wong Zhao Wu, Bryan <70313929+kiritowu@users.noreply.github.com>
2023-12-04 05:56:30 +0000

    day 4 wip

--- src/04.py
+++ src/04.py
@@ -1,9 +1,16 @@
 from typing import List
 
 def part1(lines: List[str]):
-    pass
+    total = 0
+    for line in lines:
+        game_id, numbers = line.split(": ")
+        win_nums, draw_nums = map(lambda x: set(map(int, x.split())),numbers.split(" | "))
+        score = 2 ** max(len(win_nums & draw_nums)-1,0)
+        total += score
+    return total
+
 
 if __name__ == "__main__":
     with open("../inputs/04.txt", "r") as f:
         inputs = f.read().splitlines()
-    part1(inputs)
+    print(part1(inputs))
