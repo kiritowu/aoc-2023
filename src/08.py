84a0c7897d2aa6fa24dc3158dae35de17d83dd67
Wong Zhao Wu (Bryan) <70313929+kiritowu@users.noreply.github.com>
2023-12-08 20:17:32 +0800

    Add day 8

---
+++ src/08.py
@@ -0,0 +1,126 @@
+import math
+from typing import Dict, List
+
+from aocd import get_data
+
+
+def part1(lines: List[str], start: str = "AAA", end: str = "ZZZ") -> int:
+    """
+    It seems like you're meant to use the left/right instructions to
+    navigate the network. Perhaps if you have the camel follow the same
+    instructions, you can escape the haunted wasteland!
+
+    After examining the maps for a bit, two nodes stick out: AAA and ZZZ. You
+    feel like AAA is where you are now, and you have to follow the left/right
+    instructions until you reach ZZZ.
+
+    This format defines each node of the network individually. For example:
+
+    RL
+
+    AAA = (BBB, CCC)
+    BBB = (DDD, EEE)
+    CCC = (ZZZ, GGG)
+    DDD = (DDD, DDD)
+    EEE = (EEE, EEE)
+    GGG = (GGG, GGG)
+    ZZZ = (ZZZ, ZZZ)
+
+    Starting with AAA, you need to look up the next element based on the next
+    left/right instruction in your input. In this example, start with AAA and
+    go right (R) by choosing the right element of AAA, CCC. Then, L means to
+    choose the left element of CCC, ZZZ. By following the left/right
+    instructions, you reach ZZZ in 2 steps.
+
+    Of course, you might not find ZZZ right away. If you run out of left/right
+    instructions, repeat the whole sequence of instructions as necessary: RL
+    really means RLRLRLRLRLRLRLRL... and so on.
+
+    Starting at AAA, follow the left/right instructions.
+    How many steps are required to reach ZZZ?
+    """
+    instructions = lines[0]
+    ins2idx = {"L": 0, "R": 1}
+    step = 0
+    puzzle: Dict[str, List[str]] = {}
+    for line in lines[2:]:
+        key, paths = line.split(" = ")
+        puzzle[key] = paths.replace("(", "").replace(")", "").split(", ")
+
+    state = start
+    while True:
+        ins = instructions[step % len(instructions)]
+        step += 1
+        state = puzzle[state][ins2idx[ins]]
+
+        if state == end or (end != "ZZZ" and state.endswith(end)):
+            return step
+
+
+def part2(lines: List[str]):
+    """The sandstorm is upon you and you aren't any closer to escaping the
+    wasteland. You had the camel follow the instructions, but you've barely
+    left your starting position. It's going to take significantly more steps
+    to escape!
+
+    What if the map isn't for people - what if the map is for ghosts? Are
+    ghosts even bound by the laws of spacetime? Only one way to find out.
+
+    After examining the maps a bit longer, your attention is drawn to a
+    curious fact: the number of nodes with names ending in A is equal to the
+    number ending in Z! If you were a ghost, you'd probably just start at
+    every node that ends with A and follow all of the paths at the same time
+    until they all simultaneously end up at nodes that end with Z.
+
+    For example:
+
+    LR
+
+    11A = (11B, XXX)
+    11B = (XXX, 11Z)
+    11Z = (11B, XXX)
+    22A = (22B, XXX)
+    22B = (22C, 22C)
+    22C = (22Z, 22Z)
+    22Z = (22B, 22B)
+    XXX = (XXX, XXX)
+
+    Here, there are two starting nodes, 11A and 22A (because they both end
+    with A). As you follow each left/right instruction, use that instruction
+    to simultaneously navigate away from both nodes you're currently on.
+    Repeat this process until all of the nodes you're currently on end with Z.
+    (If only some of the nodes you're on end with Z, they act like any other
+    node and you continue as normal.) In this example, you would proceed as
+    follows:
+
+        Step 0: You are at 11A and 22A.
+        Step 1: You choose all of the left paths, leading you to 11B and 22B.
+        Step 2: You choose all of the right paths, leading you to 11Z and 22C.
+        Step 3: You choose all of the left paths, leading you to 11B and 22Z.
+        Step 4: You choose all of the right paths, leading you to 11Z and 22B.
+        Step 5: You choose all of the left paths, leading you to 11B and 22C.
+        Step 6: You choose all of the right paths, leading you to 11Z and 22Z.
+
+    So, in this example, you end up entirely on nodes that end in Z after 6
+    steps.
+
+    Simultaneously start on every node that ends with A. How many steps does
+    it take before you're only on nodes that end with Z?
+    """
+    starts = []
+    steps = []
+    for line in lines[2:]:
+        key = line.split(" = ")[0]
+        if key.endswith("A"):
+            starts.append(key)
+
+    for start in starts:
+        steps.append(part1(lines, start, "Z"))
+
+    return math.lcm(*steps)
+
+
+if __name__ == "__main__":
+    data = get_data(year=2023, day=8).splitlines()
+    print(part1(data))
+    print(part2(data))
